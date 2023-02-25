2bdc369b7fb7cbb626d5edfc908ed7965c019ce6
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-02-25 19:27:47 +0700

    demo froggy and flower game

---
+++ src/flower/javascript.js
@@ -0,0 +1,13 @@
+const buttonElement = document.querySelector('button');
+
+function addCSS(){
+    const textAreaElement = document.querySelector('textarea')
+    const css = textAreaElement.value;
+    console.log(css);
+    const foreFront= document.querySelector('#forefront');
+    foreFront.style.cssText = css;
+    console.log(foreFront);
+
+
+}
+buttonElement.addEventListener('click', addCSS);
