ce8f0f8ac952b528fe662d45ef50439b06a99287
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-03-01 15:33:57 +0700

    resolve link svg

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    item1: require('../../assets/images/frog-green.svg').default,
+    item2: require('../../assets/images/frog-red.svg').default,
+   };
+   export default images;
+   
--- src/components/ScreenGame/index.js
+++ src/components/ScreenGame/index.js
@@ -1,19 +1,19 @@
 import classNames from "classnames/bind";
 import Item from "./Item";
 import styles from "./ScreenGame.module.scss";
-
+import images from "../../assets/images";
 const cx = classNames.bind(styles);
 
 function ScreenGame() {
+  console.log(images.logo);
   return (
     <>
       <section className={cx("screen")}>
         <div className={cx("forefront")}>
-          <Item image="../assets/image/level1/frog-green.svg" />
-          <Item image="" />
-          <Item image="" />
-          <Item image="" />
-          <Item image="" />
+
+          {/* <Item image={images.logo} /> */}
+          <Item image={images.item1} />
+          <Item image={images.item2} />
         </div>
         <div className={cx("background")}>
           <Item image="" />
